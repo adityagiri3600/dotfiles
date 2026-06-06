cc8f561e7cdf8b0bbbd121435de9d97df99991f8
Aditya Giri <adityagiri3600@gmail.com>
2026-06-06 22:50:03 +0530

    add: waybar improvements

--- waybar/clock-jp.sh
+++ waybar/clock-jp.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # Simple Waybar custom script that prints time + weekday in kanji as JSON
+
 kanji=(日 月 火 水 木 金 土)
 w=$(date +%w)
 weekday=${kanji[w]}
 time=$(date +%H:%M)
 tooltip=$(date +"%d/%m/%Y  %H:%M:%S")
-printf '%s' "{\"text\":\"${time} ${weekday}\",\"tooltip\":\"<span>${tooltip}</span>\"}"
+
+printf '%s\n' "{\"text\":\"<span size='12000'>${time}</span>               <span size='18000'>${weekday}</span>\",\"tooltip\":\"<span>${tooltip}</span>\"}"
